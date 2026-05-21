a8e0ae18b92ebf2a83947b82f1734a8242591bc9
deepakpatil261097 <deepakpatil261097@gmail.com>
2026-05-21 16:37:13 +0000

    Initial ERP inventory setup

---
+++ inventory/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Project, Material, StockTransaction
+
+admin.site.register(Project)
+admin.site.register(Material)
+admin.site.register(StockTransaction)
---
+++ inventory/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+
+class Project(models.Model):
+    project_code = models.CharField(max_length=50)
+    project_name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.project_name
+
+
+class Material(models.Model):
+    material_name = models.CharField(max_length=100)
+    material_code = models.CharField(max_length=50)
+    unit = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.material_name
+
+
+class StockTransaction(models.Model):
+    TRANSACTION_TYPES = (
+        ('IN', 'IN'),
+        ('OUT', 'OUT'),
+    )
+
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    material = models.ForeignKey(Material, on_delete=models.CASCADE)
+    transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
+    quantity = models.FloatField()
+
+    def __str__(self):
+        return f"{self.project} - {self.material}"
